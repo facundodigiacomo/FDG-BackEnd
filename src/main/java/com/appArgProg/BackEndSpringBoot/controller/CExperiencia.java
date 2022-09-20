29285c5a7f8765d9a376d4add298cbf266d1db9e
Facundo Di Giacomo <facundodigiacomo14@gmail.com>
2022-09-19 21:38:52 -0400

    url updated 1

--- src/main/java/com/appArgProg/BackEndSpringBoot/controller/CExperiencia.java
+++ src/main/java/com/appArgProg/BackEndSpringBoot/controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("explab")
+@RequestMapping("/explab")
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://argprog-frontend-cb7e8.web.app")
 public class CExperiencia {
